ef7a0e66c6a0cfba21540e48193afbcdc9c06695
Александр <trigun1000@mail.ru>
2021-04-20 23:33:34 +0300

    les4 docker

--- shop-ui/src/main/java/geek/service/CartServiceImpl.java
+++ shop-ui/src/main/java/geek/service/CartServiceImpl.java
@@ -64,6 +64,7 @@ public class CartServiceImpl implements CartService, Serializable {
 
     @Override
     public void removeProduct(Long productId) {
+        logger.info("");
         lineItems = lineItems
                 .entrySet()
                 .stream()
